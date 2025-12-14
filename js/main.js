8739aaaa5a9774554700651ddcc491e477ee1109
bananna <annabalan267@gmail.com>
2025-12-14 18:18:39 +0300

    added barchart

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-alert("Hello!")
-var buttonElement = document.querySelector("button");
-var textElement = document.querySelector("p");
-
-buttonElement.addEventListener("click", changeText, false);
-
-function changeText(e) {
-    textElement.innerText = "hello, world!";
+function renderSkillsChart() {
+  const chart = echarts.init(document.getElementById('skills-chart'));
+  chart.setOption({
+    title: { text: 'Мои навыки' },
+    xAxis: { type: 'value' },
+    yAxis: {
+      type: 'category',
+      data: ['PIX BI', 'SQL', 'Modus']
+    },
+    series: [{
+      type: 'bar',
+      data: [8, 5, 3]
+    }]
+  });
 }
+
+renderSkillsChart();
